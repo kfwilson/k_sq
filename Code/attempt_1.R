f7071c7f65f1935aec0cb355226e550c815a0ec4
Katherine Wilson <kwilson@Kates-MacBook-Pro.local>
2018-02-26 22:50:34 -0800

    added simple imputation of missing values as mode. added interaction terms to model

--- Code/attempt_1.R
+++ Code/attempt_1.R
@@ -57,15 +57,18 @@ train_dta <- read.csv("./Data/train.csv")
 # FF2 you make transactions or someone else (lots of missingness but for complete obs, cor = .13)
 # GN1 who decides how money you earn will be used cor .05
 
-dta <- train_dta %>% select(train_id, is_female, DG3, DG3A, DG4, DG6, DL0, MT1A, FF1, FF2, GN1)
+dta <- train_dta %>% select(train_id, is_female, DG3, DG3A, DG4, DG6, DL0, MT1A, FF1, FF2, GN1,DL1,MT6)
 for (col in colnames(dta)[3:length(colnames(dta))]) {
-  dta[,col] <- as.factor(ifelse(dta[,col] >= 96, NA, dta[,col]))
+  dta[,col] <- ifelse(dta[,col] >= 96, NA, dta[,col])
+  ## Simple imputation of missing as mode
+  dta[,col] <- ifelse(is.na(dta[,col]), names(table(dta[,col]))[table(dta[,col])==max(table(dta[,col]))], dta[,col])
+  dta[,col] <- as.factor(dta[,col])
 }
 
 set.seed(10) # setting seed so sample can be reproduced
 select_training <- sample.int(n=nrow(dta), size =floor(.1*nrow(dta)), replace=F) # exclude these obs from our model so we can test out of sample predicting
 
-model <- glm(is_female ~ DG3 + DG4 + DG6 + DL0 + MT1A + FF1 + GN1, family=binomial(link='probit'), data = dta[select_training,])
+model <- glm(is_female ~ DG3 + DG4 + DG6 + DL0 + MT1A + FF1 + GN1 + FF2 + DL1 + MT6, family=binomial(link='probit'), data = dta[select_training,])
 # giving an error for FF2 about can only use factors with 2 or more levels???
 #summary(model)
 
@@ -73,6 +76,21 @@ oos <- dta[-select_training,]
 oos$predict_prob <- predict(model, oos, type='response')
 
 results <- data.frame()
-for (threshold in c(.4, .41, .42, .43, .44, .45, .46, .47, .48, .49, .5, .55, .6, .65, .7, .75)) {
+for (threshold in c(.3, .31, .32, .33, .34, .35, .36, .37, .38, .39, .4, .41, .42, .43, .44, .45, .46, .47, .48, .49, .5, .55, .6, .65, .7, .75)) {
   results <- correct.predictions(model, select_training, threshold, results)
 }
+
+### TEST DATA #####
+test_dta <- read.csv("./Data/test.csv")
+model <- glm(is_female ~ DG3 + DG4 + DG6 + DL0 + MT1A + FF1 + GN1 + FF2, family=binomial(link='probit'), data = dta)
+vars <- c("train_id", "is_female", "DG3", "DG3A", "DG4", "DG6", "DL0", "MT1A", "FF1", "FF2", "GN1", "DL1", "MT6")
+for (col in vars[3:length(vars)]) {
+  test_dta[,col] <- ifelse(test_dta[,col] >= 96, NA, test_dta[,col])
+  ## Simple imputation of missing as mode
+  test_dta[,col] <- ifelse(is.na(test_dta[,col]), names(table(test_dta[,col]))[table(test_dta[,col])==max(table(test_dta[,col]))], test_dta[,col])
+  test_dta[,col] <- as.factor(test_dta[,col])
+}
+test_dta$is_female <- ifelse(predict(model, test_dta) >= .31, 1, 0)
+
+write.csv(select(test_dta, "test_id", "is_female"), "./Data/submission_02.26.18.csv", row.names=F)
+
